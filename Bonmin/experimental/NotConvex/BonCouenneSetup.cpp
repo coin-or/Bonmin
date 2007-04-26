71b2d7c733c5b3e03346c3edad04909ca60731a7
Pierre Bonami <pierre.bonami@gmail.com>
2007-04-26 18:00:37 +0000

    Try to free ASL object cleanly

--- Bonmin/experimental/NotConvex/BonCouenneSetup.cpp
+++ Bonmin/experimental/NotConvex/BonCouenneSetup.cpp
@@ -23,7 +23,28 @@
 namespace Bonmin{
   
   SmartAsl::~SmartAsl(){
-    if(asl != NULL)
+    //Code from Ipopt::AmplTNLP to free asl
+    if(asl != NULL){
+        if (X0) {
+          delete [] X0;
+          X0 = NULL;
+        }
+        if (havex0) {
+          delete [] havex0;
+          havex0 = NULL;
+        }
+        if (pi0) {
+          delete [] pi0;
+          pi0 = NULL;
+        }
+        if (havepi0) {
+          delete [] havepi0;
+          havepi0 = NULL;
+        }
+        ASL* asl_to_free = (ASL*)asl;
+        ASL_free(&asl_to_free);
+        asl = NULL;
+      }
       ASL_free(&asl);
   }
   
