66c686bd62e1bddbac3cea9b3074894973bc9bf3
Pietro Belotti <petr7b6@gmail.com>
2007-07-18 14:30:53 +0000

    added code for linear part in quadCuts.cpp

--- Couenne/src/convex/operators/conv-exprQuad.cpp
+++ Couenne/src/convex/operators/conv-exprQuad.cpp
@@ -1,7 +1,7 @@
 /*
  * Name:    conv-exprQuad.cpp
  * Author:  Pietro Belotti
- * Purpose: implementation of convexification methods for exprGroup
+ * Purpose: implementation of convexification methods for exprQuad
  *
  * (C) Pietro Belotti 2007. This file is licensed under the Common Public License (CPL)
  */
@@ -9,9 +9,7 @@
 #include <OsiRowCut.hpp>
 #include <OsiCuts.hpp>
 
-#include <exprGroup.hpp>
-#include <exprBound.hpp>
-#include <exprMul.hpp>
+#include <exprQuad.hpp>
 
 #include <CouenneProblem.hpp>
 #include <CouenneCutGenerator.hpp>
@@ -19,56 +17,12 @@
 /// Get lower and upper bound of an expression (if any)
 void exprQuad::getBounds (expression *&lb, expression *&ub) {
 
-  expression *lbnl, *ubnl;
+  expression *lbgrp, *ubgrp;
 
   // compute lower/upper bound of nonlinear part
-  exprSum::getBounds (lbnl, ubnl);
+  exprGroup::getBounds (lbgrp, ubgrp);
 
-  // count linear and constant terms
-  int nlin = 0;
-  if (fabs (c0_) > COUENNE_EPS) nlin++;
-  for (register int *ind = index_; *ind++>=0; nlin++);
-
-  expression 
-    **linall = new expression * [nlin + 1], // linear arglist for lower bound
-    **linalu = new expression * [nlin + 1]; //                    upper
-
-  // add constant to bounds
-  if (fabs (c0_) > COUENNE_EPS) {
-    *linall++ = new exprConst (c0_);
-    *linalu++ = new exprConst (c0_);
-  }
-
-  for (register int *ind = index_, i=0; *ind>=0;) {
-
-    CouNumber coeff = coeff_ [i++];
-
-    expression *l = new exprLowerBound (*ind),
-               *u = new exprUpperBound (*ind++);
-
-    if (fabs (coeff - 1.) < COUENNE_EPS) {
-      *linall++ = l;
-      *linalu++ = u;
-    } else {
-
-      expression *c1 = new exprConst (coeff),
-                 *c2 = new exprConst (coeff);
-
-      if (coeff < 0) {
-	*linall++ = new exprMul (c1, u);
-	*linalu++ = new exprMul (c2, l);
-      } else {
-	*linall++ = new exprMul (c1, l);
-	*linalu++ = new exprMul (c2, u);
-      }
-    }
-  }
-
-  *linall = lbnl;
-  *linalu = ubnl;
-
-  lb = new exprSum (linall - nlin, nlin + 1);
-  ub = new exprSum (linalu - nlin, nlin + 1);
+  ///
 }
 
 
@@ -78,71 +32,11 @@ void exprQuad::generateCuts (exprAux *w, const OsiSolverInterface &si,
 			     t_chg_bounds *chg, 
 			     int wind, CouNumber lb, CouNumber ub) {
 
-  // very similar to exprSum::generateCuts. First of all, this has
-  // been standardized into a sum, so it only gets a cut in the
-  // initial relaxation
-  if (!(cg -> isFirst ()))
-    return;
+  // see if it is necessary to create/renew the alpha-convexification
+  alphaConvexify (si);
 
-  // there is one linear term so far: -w
-  int nterms = 0;
-
-  OsiRowCut *cut = new OsiRowCut;
-
-  // count terms in linear part
-  for (register int *ind = index_; *ind++ >= 0; nterms++);
-
-  int displacement = (wind < 0) ? 1: 0;
-
-  CouNumber *coeff = new CouNumber [nargs_ + nterms + displacement];
-  int       *index = new int       [nargs_ + nterms + displacement];
-
-  //  CouNumber rhs = - c0_;
-
-  if (wind < 0) {
-    // first, make room for aux variable
-    coeff [0] = -1.; 
-    index [0] = w -> Index ();
-    lb = ub = 0;
-  }
-
-  lb -= c0_;
-  ub -= c0_;
-
-  // now add linear terms
-  for (register int i=0; i<nterms; i++) {
-
-    coeff [i + displacement] = coeff_ [i]; 
-    index [i + displacement] = index_ [i];
-  }
-
-  // scan arglist for (aux) variables and constants
-  for (register int i=0; i<nargs_; i++) {
-
-    expression *curr = arglist_ [i];
-
-    if (curr -> Type () == CONST) {// constant term in sum
-      lb -= curr -> Value ();
-      ub -= curr -> Value ();
-    }
-    else {                        // variable
-      coeff [++nterms] = 1.; 
-      index   [nterms] = curr -> Index ();
-    }
-  }
-
-  cut -> setRow (nterms + displacement, index, coeff);
-
-  delete [] index;
-  delete [] coeff;
-
-  if (lb > -COUENNE_INFINITY) cut -> setLb (lb);
-  if (ub <  COUENNE_INFINITY) cut -> setUb (ub);
-
-  // added only once, it is global
-  cut -> setGloballyValid ();
-
-  cs.insert (cut);
-
-  delete cut;
+  // based on the information (dIndex_, dCoeffLo_, dCoeffUp_)
+  // created/modified by alphaConvexify(), create convexification cuts
+  // for this expression
+  quadCuts (cs, cg);
 }
--- Couenne/src/convex/operators/quadCuts.cpp
+++ Couenne/src/convex/operators/quadCuts.cpp
@@ -15,4 +15,58 @@
 /// convexification cuts to the OsiCuts structure
 void exprQuad::quadCuts (OsiCuts &cs, CouenneCutGenerator *cg) {
 
+  /// retrieve linear coefficients/indices and constant from member of
+  /// the base class, exprGroup
+
+  int  nterms = nlterms_ + nargs_,
+      *index  = new int [nterms];
+
+  CouNumber *coeff   = new CouNumber [nterms],
+             a0      = c0_;
+
+  // linear part of exprGroup
+  for (nterms = 0; nterms < nlterms_; nterms++) {
+    index [nterms] = index_ [nterms];
+    coeff [nterms] = coeff_ [nterms];
+  }
+
+  // deal with nonlinear part (has been standardized into sum of
+  // auxiliary variables)
+
+  for (int i=0; i<nargs_; i++) {
+
+    int ind = arglist_ [i] -> Index ();
+
+    if (ind >= 0) {
+
+      index [nterms]   = ind;
+      coeff [nterms++] = 1.;
+
+    } else 
+      if (arglist_ [i] -> Type () == CONST) // if not a constant, quit
+	a0 += arglist_ [i] -> Value ();
+      else {
+	printf ("non constant, non variable term in standardized exprSum\nAborting.\n");
+	exit (-1);
+      }
+  }
+
+  // Pierre: now index[], coeff[], and a0 contain the linear data of
+  // expression a0 + a^Tx + x^T Q x. Variable nterms contains the
+  // number of linear terms.
+  //
+  // The convexification cuts should take into account the linear data
+  // as well, so if we have an auxiliary variable
+  //
+  // w = a0 + ax + x'Qx
+  //
+  // the relative lower envelope cut should be of the form 
+  //
+  // w >= a0 + ax + b + bx
+  //
+  // where w >= b + bx is the convexification cut of the quadratic
+  // expression alone
+
+  delete [] coeff;
+  delete [] index;
 }
