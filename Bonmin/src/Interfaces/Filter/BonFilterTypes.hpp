9ce2ff6ba14a7c8aaa74792ff01b70da84c9d5c2
Pierre Bonami <pierre.bonami@gmail.com>
2008-04-16 19:40:57 +0000

    Add missing file

---
+++ Bonmin/src/Interfaces/Filter/BonFilterTypes.hpp
@@ -0,0 +1,10 @@
+#ifndef BonFilterTypes_H
+#define BonFilterTypes_H
+#include "IpoptConfig.h"
+namespace FilterTypes {
+    /** Fortran type for integer used in filter. */
+    typedef ipfint fint;
+    /** Fortran type for double.used in filter */
+    typedef double real;
+}
+#endif
