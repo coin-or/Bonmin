8e967bec4ed18a7fcce1708b860bb8715cf50424
Pierre Bonami <pierre.bonami@gmail.com>
2008-11-07 15:40:41 +0000

    Create stable 1.0

--- Bonmin/examples/CppExample/MyBonmin.cpp
+++ Bonmin/examples/CppExample/MyBonmin.cpp
@@ -58,7 +58,7 @@ int main (int argc, char *argv[])
   bonmin.readOptionsFile();// This reads the default file "bonmin.opt"
   
   // Options can also be set by using a string with a format similar to the bonmin.opt file
-  bonmin.readOptionsString("bonmin.algorithm B-BB\n");
+//  bonmin.readOptionsString("bonmin.algorithm B-BB\n");
   
   // Now we can obtain the value of the new option
   int printSolution;
