29ba1a8f4588bfc83faa4e974a3af72948ed3fcc
Pierre Bonami <pierre.bonami@gmail.com>
2009-02-18 13:34:59 +0000

    Forgotten pump file

---
+++ Bonmin/src/CbcBonmin/Heuristics/BonPumpForMinlp.cpp
@@ -0,0 +1,79 @@
+// (C) Copyright CNRS
+// This code is published under the Common Public License.
+//
+// Authors :
+// Pierre Bonami, LIF Université de la Méditérannée-CNRS
+//
+// Date : 02/18/2009
+
+#include "BonPumpForMinlp.hpp"
+#include "CbcModel.hpp"
+#include "OsiBranchingObject.hpp"
+
+namespace Bonmin {
+
+  /** Default constructor*/
+  PumpForMinlp::PumpForMinlp():
+    LocalSolverBasedHeuristic(){
+  }
+  /** Constructor with setup.*/
+  PumpForMinlp::PumpForMinlp(BonminSetup * setup):
+    LocalSolverBasedHeuristic(setup){
+    setupDefaults(setup->options());
+  }
+
+  /** Copy constructor.*/
+  PumpForMinlp::PumpForMinlp
+             (const PumpForMinlp &other):
+    LocalSolverBasedHeuristic(other){
+  }
+
+  PumpForMinlp::~PumpForMinlp(){
+  }
+
+  /** Runs heuristic*/
+  int
+  PumpForMinlp::solution(double & objectiveValue,
+                                 double * newSolution){
+    if(model_->getNodeCount() || model_->getCurrentPassNumber() > 1) return 0;
+    int numberObjects = model_->numberObjects();
+    OsiObject ** objects = model_->objects();
+    OsiTMINLPInterface * nlp = dynamic_cast<OsiTMINLPInterface *>
+                               (setup_->nonlinearSolver()->clone());
+
+    OsiBranchingInformation info = model_->usefulInformation();
+
+    double cutoff = info.cutoff_; 
+    int r_val = doLocalSearch(nlp, newSolution, objectiveValue, cutoff, "pump_for_minlp.");
+    delete nlp;
+    return r_val;
+  }
+
+   void
+   PumpForMinlp::setupDefaults(Ipopt::SmartPtr<Bonmin::OptionsList> options){
+     int dummy;
+     printf("Changing defaults PumpForMinlp\n");
+     std::string prefix = "pump_for_minlp.";
+     changeIfNotSet(options, prefix, "algorithm", "B-iFP");
+     changeIfNotSet(options, prefix, "time_limit", 60.);
+   }
+
+
+  void
+  PumpForMinlp::registerOptions(Ipopt::SmartPtr<Bonmin::RegisteredOptions> roptions){
+   roptions->SetRegisteringCategory("Local search based heuristics", RegisteredOptions::BonminCategory);
+   roptions->AddStringOption2(
+     "pump_for_minlp",
+     "if yes runs a heuristic which looks like a dummy FP",
+     "no",
+     "no", "don't run it",
+     "yes", "runs the heuristic",
+     "");
+  }
+
+   /** Initiaize using passed options.*/
+   void 
+   PumpForMinlp::Initialize(Ipopt::SmartPtr<Bonmin::OptionsList> options){
+   }
+}/* ends bonmin namespace*/
+
