3f7c9a4fc81521d8155aee2909a2b45c96d69c4c
Pierre Bonami <pierre.bonami@gmail.com>
2009-11-16 18:25:29 +0000

    Put back cut effectiveness

--- Bonmin/src/Interfaces/BonOsiTMINLPInterface.cpp
+++ Bonmin/src/Interfaces/BonOsiTMINLPInterface.cpp
@@ -1863,7 +1863,7 @@ OsiTMINLPInterface::getOuterApproximation(OsiCuts &cs, const double * x, bool ge
     if(global) {
       newCut.setGloballyValidAsInteger(1);
     }
-    //newCut.setEffectiveness(99.99e99);
+    newCut.setEffectiveness(99.99e99);
     newCut.setLb(lb[cutIdx]);
     newCut.setUb(ub[cutIdx]);
     newCut.setRow(cuts[cutIdx]);
@@ -1927,7 +1927,7 @@ OsiTMINLPInterface::getOuterApproximation(OsiCuts &cs, const double * x, bool ge
       OsiRowCut newCut;
       if(global)
 	newCut.setGloballyValidAsInteger(1);
-      //newCut.setEffectiveness(99.99e99);
+      newCut.setEffectiveness(99.99e99);
       newCut.setRow(v);
       newCut.setLb(-COIN_DBL_MAX/*Infinity*/);
       newCut.setUb(ub[nNonLinear_]);
