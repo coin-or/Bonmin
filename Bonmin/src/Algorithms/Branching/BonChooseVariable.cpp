4964ab08ab019d778fcd4624549b3a3c7c9360e7
Pierre Bonami <pierre.bonami@gmail.com>
2011-05-22 17:39:28 +0000

    Fix for couenne suggested by Francois now in trunk

--- Bonmin/src/Algorithms/Branching/BonChooseVariable.cpp
+++ Bonmin/src/Algorithms/Branching/BonChooseVariable.cpp
@@ -1032,6 +1032,9 @@ BonChooseVariable::updateInformation( int index, int branch,
   if(cbc_model_ == NULL) return;
   assert (index<solver_->numberObjects()); 
   assert (branch<2); 
+
+  if(fabs(changeInValue) < 1e-6) return;
+
   double* upTotalChange = pseudoCosts_.upTotalChange(); 
   double* downTotalChange = pseudoCosts_.downTotalChange(); 
   int* upNumber = pseudoCosts_.upNumber(); 
