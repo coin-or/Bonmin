79fe563c4d727fdc514a6a0a89be058d52fbf9ca
Pierre Bonami <pierre.bonami@gmail.com>
2011-06-13 14:38:13 +0000

    For Filter

--- Bonmin/src/Interfaces/Filter/BonFilterTypes.hpp
+++ Bonmin/src/Interfaces/Filter/BonFilterTypes.hpp
@@ -3,7 +3,7 @@
 #include "IpoptConfig.h"
 namespace FilterTypes {
     /** Fortran type for integer used in filter. */
-    typedef ipfint fint;
+    typedef FORTRAN_INTEGER_TYPE fint;
     /** Fortran type for double.used in filter */
     typedef double real;
 }
