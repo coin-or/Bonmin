a8c01b181b8b6a920e84aa4762eeb6d1a509d9c1
Pierre Bonami <pierre.bonami@gmail.com>
2011-06-24 13:38:34 +0000

    Merge documentation fix

--- Bonmin/examples/CppExample/MyTMINLP.hpp
+++ Bonmin/examples/CppExample/MyTMINLP.hpp
@@ -15,11 +15,11 @@ using namespace Bonmin;
    * This class implements the following NLP :
   * \f[ 
     \begin{array}{l}
-    \min x_0 - x_1 - x_2 \\ 
+    \min - x_0 - x_1 - x_2 \\ 
     \mbox{s.t}\\
     (x_1 - \frac{1}{2})^2 + (x_2 - \frac{1}{2})^2 \leq \frac{1}{4} \\
     x_0 - x_1 \leq 0 \\
-    x_1 + x_2 + x_3 \leq 2\\
+    x_0 + x_2 + x_3 \leq 2\\
     x_0 \in \{0,1\}^n \; (x_1, x_2) \in R^2 \; x_3 \in N
     \end{array}
     \f]
