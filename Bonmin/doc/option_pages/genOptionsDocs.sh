49ba5bdd132a1cfa9b07ca5cc8eec60139933038
Pierre Bonami <pierre.bonami@gmail.com>
2011-07-25 12:57:55 +0000

    Add scripts to document options

---
+++ Bonmin/doc/option_pages/genOptionsDocs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+categories="bonmin filter ipopt"
+
+for f in $categories ; do
+  awk -v NAME=$f '{sub(/TEMPLATE/,NAME) ; print}' options_list_TEMPLATE.tex > options_list_$f.tex
+  ./genHtml.sh options_list_$f.tex
+  mv tmp.html options_list_$f.html
+done
