6b42950205db7d33c3ee12ae17af48bf464948cc
Pierre Bonami <pierre.bonami@gmail.com>
2011-10-13 13:43:20 +0000

    Remove printfs

--- Bonmin/src/CbcBonmin/Heuristics/BonMilpRounding.cpp
+++ Bonmin/src/CbcBonmin/Heuristics/BonMilpRounding.cpp
@@ -84,7 +84,6 @@ namespace Bonmin
   int
   MilpRounding::solution(double &solutionValue, double *betterSolution)
   {
-    printf("Entering heuristic\n");
     if(model_->getCurrentPassNumber() > 1) return 0;
     if ((model_->getNodeCount()%howOften_)!=0||model_->getCurrentPassNumber()>1)
       return 0;
@@ -242,12 +241,10 @@ namespace Bonmin
                       newRowLower(), newRowUpper());
       si->setInteger(idxIntegers(), static_cast<int>(idxIntegers.size()));
       si->applyCuts(noGoods);
-      printf("Done creating mip, start solving\n"); 
 
       bool hasFractionnal = true;
       while(hasFractionnal){
         mip_->optimize(DBL_MAX, 0, 60);
-        printf("Done solving\n"); 
         hasFractionnal = false;
 #if 0
         bool feasible = false;
@@ -301,7 +298,6 @@ namespace Bonmin
     if(feasible) {
       // fix the integer variables and solve the NLP
       // also add no good cut
-      printf("We found a solution");
       CoinPackedVector v;
       double lb = 1;
       for (int iColumn=0;iColumn<n;iColumn++) {
@@ -334,13 +330,11 @@ namespace Bonmin
       if(feasible) {
 	nlp->initialSolve();
 	if(minlp->optimization_status() != Ipopt::SUCCESS) {
-          printf("Infeasible for NLP");
 	  feasible = false;
 	}
 	std::copy(x_sol,x_sol+n, newSolution.begin());
       }
     }
-    else {printf("No solution found\n");}
     if(feasible) {
       double newSolutionValue;
       minlp->eval_f(n, newSolution(), true, newSolutionValue); 
