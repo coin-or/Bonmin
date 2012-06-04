8527029ce789ed824bb2c7e38b06e9e1a41560f2
Pierre Bonami <pierre.bonami@gmail.com>
2012-06-04 12:58:11 +0000

    temporarily committing release candidate

--- Bonmin/src/Interfaces/config_bonmin_default.h
+++ Bonmin/src/Interfaces/config_bonmin_default.h
@@ -5,7 +5,7 @@
 /***************************************************************************/
 
 /* Version number of project */
-#define BONMIN_VERSION "1.6"
+#define BONMIN_VERSION "1.6.0"
 
 /* Major Version number of project */
 #define BONMIN_VERSION_MAJOR 1
@@ -14,4 +14,4 @@
 #define BONMIN_VERSION_MINOR 6
 
 /* Release Version number of project */
-#define BONMIN_VERSION_RELEASE 9999
+#define BONMIN_VERSION_RELEASE 0
