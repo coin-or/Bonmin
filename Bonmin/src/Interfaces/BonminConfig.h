1824c2417f6023e79d989ccbadcd7aab7e869017
Stefan Vigerske <svigerske@gams.com>
2014-12-13 12:08:57 +0000

    BonminConfig should be CPL too

--- Bonmin/src/Interfaces/BonminConfig.h
+++ Bonmin/src/Interfaces/BonminConfig.h
@@ -1,6 +1,6 @@
 /* Copyright (C) 2011
  * All Rights Reserved.
- * This code is published under the Eclipse Public License.
+ * This code is published under the Common Public License.
  *
  * $Id$
  *
