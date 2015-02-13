c40fe7aeac9b0f09088c6eaeeb3fb1d7074de4b5
Stefan Vigerske <svigerske@gams.com>
2015-02-13 07:21:44 +0000

    restoring Bonmin/stable/1.8

--- Bonmin/src/Interfaces/config_bonmin_default.h
+++ Bonmin/src/Interfaces/config_bonmin_default.h
@@ -5,7 +5,7 @@
 /***************************************************************************/
 
 /* Version number of project */
-#define BONMIN_VERSION "1.8.1"
+#define BONMIN_VERSION "1.8"
 
 /* Major Version number of project */
 #define BONMIN_VERSION_MAJOR 1
@@ -14,4 +14,4 @@
 #define BONMIN_VERSION_MINOR 8
 
 /* Release Version number of project */
-#define BONMIN_VERSION_RELEASE 1
+#define BONMIN_VERSION_RELEASE 9999
