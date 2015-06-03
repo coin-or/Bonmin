06c7da21df426ec3e033bbd6e9902ab5b314193d
Ted Ralphs <ted@lehigh.edu>
2015-06-03 15:41:14 +0000

    restoring Bonmin/stable/1.8

--- Bonmin/src/Interfaces/config_bonmin_default.h
+++ Bonmin/src/Interfaces/config_bonmin_default.h
@@ -5,13 +5,13 @@
 /***************************************************************************/
 
 /* Version number of project */
-#define BONMIN_VERSION "1.8.3"
+#define BONMIN_VERSION  "trunk"
 
 /* Major Version number of project */
-#define BONMIN_VERSION_MAJOR 1
+#define BONMIN_VERSION_MAJOR   9999
 
 /* Minor Version number of project */
-#define BONMIN_VERSION_MINOR 8
+#define BONMIN_VERSION_MINOR   9999
 
 /* Release Version number of project */
-#define BONMIN_VERSION_RELEASE 3
+#define BONMIN_VERSION_RELEASE 9999
