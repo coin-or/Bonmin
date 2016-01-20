039ff41533abd5cad4296ba3727938d03ac03492
Ted Ralphs <ted@lehigh.edu>
2016-01-20 19:26:25 +0000

    C++ style comments not allowed in C90

--- Bonmin/src/Interfaces/BonStdCInterface.h
+++ Bonmin/src/Interfaces/BonStdCInterface.h
@@ -1,11 +1,12 @@
-// (C) Copyright Shaurya Sharma
-// All Rights Reserved.
-// This code is published under the Eclipse Public License.
-//
-// Authors :
-// S. Sharma
-//
-// Date :  07.06.2015
+/* (C) Copyright Shaurya Sharma
+   All Rights Reserved.
+   This code is published under the Eclipse Public License.
+
+   Authors :
+   S. Sharma
+
+   Date :  07.06.2015
+*/
 
 #ifndef __BONSTDCINTERFACE_H__
 #define __BONSTDCINTERFACE_H__
